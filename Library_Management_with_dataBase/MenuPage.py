5c1f7f18b449a2e326f4ecda2d2878844e501f31
Roger that <stevethat@roger.gh>
2020-02-05 02:03:28 +0500

    Calling a class and modifying our code

--- Library_Management_with_dataBase/MenuPage.py
+++ Library_Management_with_dataBase/MenuPage.py
@@ -3,14 +3,17 @@ from tkinter import *
 
 class MenuPageClass:
     def __init__(self, root):
+
+        self.root = root
         root.title( 'Module' )  # title
         root.configure( bg = '#bcdebb' )
         root.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left))
         root.minsize( 400 , 200 )
         root.maxsize( 1100 , 700 )
-        root.iconbitmap( r'model_icon.ico' )
 
 
 
 r = Tk()
+
+Menu_class_obj = MenuPageClass(r)
 r.mainloop()
