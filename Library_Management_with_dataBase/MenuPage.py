e815800cac0a5a191dddd553199c96b0f1994874
Roger that <stevethat@roger.gh>
2020-02-05 02:05:44 +0500

    Taking some same code from login page

--- Library_Management_with_dataBase/MenuPage.py
+++ Library_Management_with_dataBase/MenuPage.py
@@ -10,7 +10,13 @@ class MenuPageClass:
         root.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left))
         root.minsize( 400 , 200 )
         root.maxsize( 1100 , 700 )
+        Label( root , text = " Note: Here you can make your study perfect... :) " , bg = "#66bd6d" , relief = "solid" ,
+               anchor = "w" , height = 2 , font = "Times%New%Roman 11 bold italic" ).pack()
 
+        # Creating Frame
+        lf1 = LabelFrame( root , text = "Enter Your choice" , fg = "red" , bg = '#33ff9e' ,
+                          relief = "solid" , font = "Times%New%Roman 16 bold" , height = 550 )
+        lf1.pack( fill = "both" , expand = True , padx = 20 , pady = 20 )
 
 
 r = Tk()
