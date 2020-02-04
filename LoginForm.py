ac48b096ac24490e5f780975453c35b32e1d7eae
Roger that <stevethat@roger.gh>
2020-02-04 23:41:52 +0500

    minimum and maximum size of window

--- LoginForm.py
+++ LoginForm.py
@@ -5,6 +5,9 @@ r = Tk()
 # Creating format
 r.title( 'Module' )  # title
 r.configure( bg = '#bcdebb' ) # Changing backgroud colour.
-r.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left)) # assigning size and position to widndow.
+r.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left)) # assigning size and position to widndow
+# minimum and maximum size of window.
+r.minsize( 400 , 200 )
+r.maxsize( 1100 , 700 )
 
 r.mainloop()
