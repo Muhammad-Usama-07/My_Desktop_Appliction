abb816142d546eb0303468eb9514cb98b7d360a9
Roger that <stevethat@roger.gh>
2020-02-04 23:40:22 +0500

    assigning size and position to widndow

--- LoginForm.py
+++ LoginForm.py
@@ -5,5 +5,6 @@ r = Tk()
 # Creating format
 r.title( 'Module' )  # title
 r.configure( bg = '#bcdebb' ) # Changing backgroud colour.
+r.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left)) # assigning size and position to widndow.
 
 r.mainloop()
