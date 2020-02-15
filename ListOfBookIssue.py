e1abc22012fc914dda728dca474e8227eef9688e
Roger that <stevethat@roger.gh>
2020-02-15 19:03:21 +0500

    Creating Window

--- ListOfBookIssue.py
+++ ListOfBookIssue.py
@@ -1,1 +1,38 @@
 from tkinter import *
+from tkinter import messagebox
+class ListBookIssueClass():
+    def __init__(self):
+        self.root = Tk()
+        self.root.title( 'Add Book' )
+        self.root.geometry( "900x600+300+50" )
+        self.root.minsize( 400 , 200 )
+        self.root.maxsize( 1100 , 700 )
+        self.root.configure( bg = '#bcdebb' )
+        Label( self.root , text = " Note: Here you can make your study perfect... :) " , bg = "#66bd6d" ,
+               relief = "solid" ,
+               anchor = "w" , height = 2 , font = "Times%New%Roman 11 bold italic" ).pack()
+
+        lf = LabelFrame( self.root , text = "Enter your Choice" , fg = "red" , bg = '#33ff9e' ,
+                         relief = "solid" , font = "Times%New%Roman 16 bold" , height = 550 )
+        lf.pack( fill = "both" , expand = True , padx = 20 , pady = 20 )
+
+        '''Functions
+          ***********'''
+
+
+        # Labels
+
+
+        '''Entries
+          ********'''
+
+
+    def ListBookIssueFunc(self):
+        self.root.mainloop()
+
+
+
+
+r = Tk()
+obj = ListBookIssueClass()
+r.mainloop()
