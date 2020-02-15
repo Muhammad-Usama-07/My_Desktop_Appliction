500c2ba8c3544e4696eefada976c4d67c3c21487
Roger that <stevethat@roger.gh>
2020-02-15 23:51:12 +0500

    Working with same code as add book

--- IssueBook.py
+++ IssueBook.py
@@ -1,1 +1,54 @@
 from tkinter import *
+from tkinter import messagebox
+class IssueBookClass():
+    def __init__(self):
+        self.root = Tk()
+        self.root.title( 'Add Book' )
+        self.root.geometry( "900x600+300+50" )
+        self.root.minsize( 400 , 200 )
+        self.root.maxsize( 1100 , 700 )
+        self.root.configure( bg = '#bcdebb' )
+        Label( self.root , text = " Note: Here you can make your study perfect... :) " , bg = "#66bd6d" ,
+               relief = "solid" ,
+               anchor = "w" , height = 2 , font = "Times%New%Roman 11 bold italic" ).pack()
+
+        lf = LabelFrame( self.root , text = "Fill the Details" , fg = "red" , bg = '#33ff9e' ,
+                         relief = "solid" , font = "Times%New%Roman 16 bold" , height = 550 )
+        lf.pack( fill = "both" , expand = True , padx = 20 , pady = 20 )
+
+        # Functions
+        # ***********
+
+        def BookIssued():
+            if ():
+                pass
+            else:
+                messagebox.showerror( "Alter" , "Enter Correct Username and password" )
+
+        # Labels
+
+
+        # Entries
+        # ********
+
+
+
+        Book_Issued_button = Button( lf , text = "Issue this Book" , bg = '#4dff4d' , font = ("Times%New%Roman" , 20 , "bold") ,
+                                  relief = "groove"
+                                  , command = BookIssued )
+        Book_Issued_button.place( x = 120 , y = 350 )
+
+        Back_button = Button( lf , text = "Back" , bg = '#4dff4d' , font = ("Times%New%Roman" , 20 , "bold") ,
+                              relief = "groove"
+                              , command = self.root.destroy )
+        Back_button.place( x = 710 , y = 350 )
+
+    def IssueBookFunc(self):
+        self.root.mainloop()
+
+
+
+
+r = Tk()
+IssueBookClass()
+r.mainloop()
