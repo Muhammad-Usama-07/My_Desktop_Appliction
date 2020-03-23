3c569fa29bbb1e85bdb7cdce438b05814d9cc8ee
Round Robin <Round@gmail.com>
2020-03-23 20:27:00 +0500

    Our Window has been modifyed finally

--- Video_download_manager_with_python.py
+++ Video_download_manager_with_python.py
@@ -7,7 +7,7 @@ import sys
 
 root = Tk()
 root.title('Video Download Manager ')
-root.geometry("900x600+300+50")
+root.geometry("900x400+300+50")
 root.minsize(400, 200)
 root.maxsize(1100, 700)
 root.configure(bg='#bcdebb')
@@ -16,8 +16,8 @@ Label(root, text=" Welcome to my Video Downloader", bg="#66bd6d",
       anchor="w", height=2, font="Times%New%Roman 11 bold italic").pack(fill=X)
 
 lf = LabelFrame(root, text="Whatr you want", fg="red", bg='#33ff9e',
-                relief="solid", font="Times%New%Roman 16 bold", height=550)
-lf.pack(fill="both", expand=True, padx=20, pady=20)
+                relief="solid", font="Times%New%Roman 16 bold", height=370)
+lf.pack(fill="both", padx=20, pady=20)
 
 '''Functions
   ************'''
@@ -52,11 +52,11 @@ def save_path():
 
 # Labels
 
-link_address_Label = Label(lf, text="Enter Your Link Address: ", bg="#33ff9e",
+link_address_Label = Label(lf, text="Link Address: ", bg="#33ff9e",
                            anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
 link_address_Label.place(x=20, y=20)
 
-Quality_Label = Label(lf, text="Select Quality: ", bg="#33ff9e",
+Quality_Label = Label(lf, text="your Location: ", bg="#33ff9e",
                       anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
 Quality_Label.place(x=20, y=70)
 
@@ -66,18 +66,20 @@ Quality_Label.place(x=20, y=70)
 link_address_var = StringVar()
 link_address_entry = Entry(lf,  textvariable = link_address_var,width=55, relief="solid",
                            font=("Times%New%Roman", 13))
-link_address_entry.place(x=220, y=30)
-
+link_address_entry.place(x=140, y=30)
+save_entry = Entry(lf,width=55, relief="solid",
+                           font=("Times%New%Roman", 13))
+save_entry.place(x=140, y=78)
 
 '''Button
   ******** '''
-save_button = Button(lf, text="Save to", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
+save_button = Button(lf, text="Select Location", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
                      relief="groove", command=save_path)
-save_button.place(x=750, y=20)
+save_button.place(x=660, y=75)
 
 download_button = Button(lf, text="Download", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
                          relief="groove", command = download)
-download_button.place(x=700, y=130)
+download_button.place(x=300, y=130)
 
 
 root.mainloop()
