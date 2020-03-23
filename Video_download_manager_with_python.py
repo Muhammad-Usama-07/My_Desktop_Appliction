444060a5756d79d7b6725a83063b5d96feaa7c93
Round Robin <Round@gmail.com>
2020-03-23 19:47:12 +0500

    Remove Class

--- Video_download_manager_with_python.py
+++ Video_download_manager_with_python.py
@@ -3,79 +3,89 @@ from tkinter import filedialog
 import pytube
 import os
 import sys
-class VDM():
-    def __init__(self):
-        self.root = Tk()
-        self.root.title( 'Video Download Manager ' )
-        self.root.geometry( "900x600+300+50" )
-        self.root.minsize( 400 , 200 )
-        self.root.maxsize( 1100 , 700 )
-        self.root.configure( bg = '#bcdebb' )
-        Label( self.root , text = " Welcome to my Video Downloader" , bg = "#66bd6d" ,
-               relief = "solid" ,
-               anchor = "w" , height = 2 , font = "Times%New%Roman 11 bold italic" ).pack(fill = X)
 
-        lf = LabelFrame(self.root, text="Whatr you want", fg="red", bg='#33ff9e',
-                        relief="solid", font="Times%New%Roman 16 bold", height=550)
-        lf.pack(fill="both", expand=True, padx=20, pady=20)
+root = Tk()
+root.title('Video Download Manager ')
+root.geometry("900x600+300+50")
+root.minsize(400, 200)
+root.maxsize(1100, 700)
+root.configure(bg='#bcdebb')
+Label(root, text=" Welcome to my Video Downloader", bg="#66bd6d",
+      relief="solid",
+      anchor="w", height=2, font="Times%New%Roman 11 bold italic").pack(fill=X)
 
-        '''Functions
-          ************'''
-        def save_path():
-            filename = filedialog.asksaveasfile()
-            if filename is None:
-                print("Any name is required")
+lf = LabelFrame(root, text="Whatr you want", fg="red", bg='#33ff9e',
+                relief="solid", font="Times%New%Roman 16 bold", height=550)
+lf.pack(fill="both", expand=True, padx=20, pady=20)
+
+'''Functions
+  ************'''
 
 
+def save_path():
+    filename = filedialog.askdirectory()
+    if filename is None:
+        print("Any name is required")
 
-        # Labels
 
-        link_address_Label = Label(lf, text="Enter Your Link Address: ", bg="#33ff9e",
-                                anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
-        link_address_Label.place(x=20, y=20)
+# Labels
 
-        Quality_Label = Label(lf, text="Select Quality: ", bg="#33ff9e",
-                                   anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
-        Quality_Label.place(x=20, y=70)
+link_address_Label = Label(lf, text="Enter Your Link Address: ", bg="#33ff9e",
+                           anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
+link_address_Label.place(x=20, y=20)
 
-        '''Quality Menu
-           ************'''
+Quality_Label = Label(lf, text="Select Quality: ", bg="#33ff9e",
+                      anchor="w", height=2, font=("Times%New%Roman", 12, "bold italic"))
+Quality_Label.place(x=20, y=70)
 
-        items = ["one","two","three","four" ]
-        select_resolution_var = StringVar()
-        select_resolution_var.set(items[0])
-
-        select_resolution = OptionMenu(lf,select_resolution_var, *items)
-        select_resolution.config(width = 90,relief="solid",font=("Times%New%Roman", 8),
-                                                                            bd = 1.3)
-        select_resolution.place(x=220, y=80)
-
-        '''Entries
+'''Entries
           ********'''
 
-        link_address_var = StringVar()
-        link_address_entry = Entry(lf, textvariable = link_address_var,width=55, relief="solid",
-                                      font=("Times%New%Roman", 13))
-        link_address_entry.place(x=220, y=30)
+link_address_var = StringVar()
+link_address_entry = Entry(lf, width=55, relief="solid",
+                           font=("Times%New%Roman", 13))
+link_address_entry.place(x=220, y=30)
 
-        yt = pytube.YouTube(link_address_var)
-        videos = yt.streams.filter(subtype='mp4', progressive=True)
+yt = pytube.YouTube(str(link_address_var))
+videos = yt.streams.filter(subtype='mp4', progressive=True)
 
-        '''Button
-          ******** '''
-        save_button = Button(lf, text="Save to", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
-                                 relief="groove", command = save_path)
-        save_button.place(x=750, y=20)
+'''Menu
+    ****'''
+items = []
+for v in videos:
+    items.append(str(v))
 
-        download_button = Button(lf, text="Download", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
-                                 relief="groove")
-        download_button.place(x=700, y=130)
+select_resolution_var = StringVar()
+select_resolution_var.set(items[0])
+
+select_resolution = OptionMenu(lf, select_resolution_var, *items)
+select_resolution.config(width=90, relief="solid", font=("Times%New%Roman", 8),
+                         bd=1.3)
+select_resolution.place(x=220, y=80)
+
+vid = select_resolution_var.get()
+
+filename = filedialog.askdirectory()
+
+dest = str(os.path.dirname(filename))
+
+if filename is None:
+    print("Any name is required")
+    vid.download(dest)
+    print("\n Downloaded succssesfully")
+    sys.exit()
+
+'''Button
+  ******** '''
+save_button = Button(lf, text="Save to", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
+                     relief="groove", command=save_path)
+save_button.place(x=750, y=20)
+
+download_button = Button(lf, text="Download", bg='#4dff4d', font=("Times%New%Roman", 15, "bold"),
+                         relief="groove")
+download_button.place(x=700, y=130)
 
 
 
 
 
-
-r = Tk()
-VDM()
-r.mainloop()
