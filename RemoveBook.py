01f8c5914deed72eaa84b570e831ba7bc00529e1
Round Robin <Round@gmail.com>
2020-03-27 00:58:29 +0500

    Modifying Code

--- RemoveBook.py
+++ RemoveBook.py
@@ -50,9 +50,9 @@ class RemoveBookClass():
         #Creating ScrollBar
 
         scroll_Bar = Scrollbar(lf, width = 25, relief = "solid")
-        scroll_Bar.place(x = 496, y = 80, height = 245)
+        scroll_Bar.place(x = 492, y = 80, height = 245)
 
-        Book_Detail = Text(lf, width = 59, height = 15, relief = "solid" ,yscrollcommand =  scroll_Bar.set)
+        Book_Detail = Listbox(lf, width = 78, height = 15, relief = "solid" ,yscrollcommand =  scroll_Bar.set)
         Book_Detail.place(x = 20, y = 80)
         scroll_Bar.config( command = Book_Detail.yview )
 
